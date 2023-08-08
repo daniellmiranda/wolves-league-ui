c97eeecaa342155a592f7353712c7fc917fbcade
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2023-08-08 11:22:41 -0300

    creates `docs` package
    Package in the monorepo that implements Storybook

---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,20 @@
+import '@wolves-league/react/dist/index.css';
+import type { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: themes.dark,
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@wolves-league/react'
+import { Cog6ToothIcon } from '@heroicons/react/24/solid'
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Forms/Button',
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    children: { control: 'text', defaultValue: 'Button Text' },
+    variant: {
+      control: 'select',
+      options: ['primary', 'secondary', 'highlight', 'iconOnly'],
+    },
+    theme: { control: 'select', options: ['light', 'dark'] },
+    disabled: { control: 'boolean' },
+    onClick: { table: { disable: true }, action: 'clicked' },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+  args: {
+    children: 'Button Text',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    children: 'Button Text',
+    variant: 'secondary',
+  },
+}
+
+export const Highlight: Story = {
+  args: {
+    children: 'Button Text',
+    variant: 'highlight',
+  },
+}
+
+export const IconOnly: Story = {
+  args: {
+    children: <Cog6ToothIcon style={{ height: '2rem', widows: '2rem' }} />,
+    variant: 'iconOnly',
+  },
+}
