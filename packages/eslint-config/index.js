d41b13b4eaf5777698a4c733b8eb631857eb6da3
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2023-08-08 21:23:08 -0300

    creates `eslint-config` package
    Package in the monorepo to be extended by other packages in the `.eslintrc.json` file to improve consistency and avoid repetition

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'prettier/prettier': ["error", {
+      printWidth: 100,
+      tabWidth: 2,
+      singleQuote: true,
+      trailingComma: 'all',
+      arrowParens: 'always',
+      semi: true,
+      endOfLine: 'auto',
+    }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  }
+}
