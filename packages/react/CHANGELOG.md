affa0ce065b4d003e8f40c522fd797c7a53a9d7e
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2023-09-04 21:10:20 -0300

    `react`: adds `forwardRef` and changes cursor

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,10 +1,13 @@
 # @wolves-league-ui/react
 
+## 1.1.1
+### `Button`
+  - Adds `forwardRef`
+  - Changes from `cursor-pointer` to `cursor-default`
+
 ## 1.1.0
-
-### Minor Changes
-
-- - Removes `button` variant `tertiary`
+### `Button`
+  - Removes `button` variant `tertiary`
   - Adds `button` variants `highlight` and `link`
   - Adds `button` sizes `sm`, `md`, `lg` and `icon`
   - Adds `asChild` prop with RadixUI `Slot`
--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,16 +1,16 @@
 import { Slot } from '@radix-ui/react-slot';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 import { VariantProps, tv } from 'tailwind-variants';
 
 const button = tv({
-  base: 'inline-flex gap-2 px-4 py-2 rounded-lg transition-all ease-in-out border border-transparent font-medium transform active:scale-95 disabled:text-gray400 disabled:border-dashed disabled:border-gray400 disabled:bg-transparent disabled:cursor-not-allowed',
+  base: 'inline-flex gap-2 px-4 py-2 rounded-lg transition-all ease-in-out border border-transparent font-medium transform cursor-default active:scale-95 disabled:text-gray400 disabled:border-dashed disabled:border-gray400 disabled:bg-transparent disabled:cursor-not-allowed',
   variants: {
     variant: {
       primary: 'bg-gray900 text-gray100 hover:bg-gray600',
       secondary:
         'dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-wl700 border-gray900 text-gray900 hover:bg-gray900 hover:text-gray100',
       highlight: 'bg-wl700 text-white hover:bg-wl900',
-      link: 'underline-offset-4 hover:underline',
+      link: 'underline-offset-4 cursor-pointer hover:underline',
     },
     size: {
       sm: 'text-sm',
@@ -29,11 +29,15 @@ interface ButtonProps extends ComponentPropsWithoutRef<'button'>, VariantProps<t
   asChild?: boolean;
 }
 
-export function Button({ children, variant, size, asChild = false, ...props }: ButtonProps) {
-  const Comp = asChild ? Slot : 'button';
-  return (
-    <Comp className={button({ variant, size })} {...props}>
-      {children}
-    </Comp>
-  );
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp className={button({ variant, size })} ref={ref} {...props}>
+        {children}
+      </Comp>
+    );
+  },
+);
+
+Button.displayName = 'Button';
