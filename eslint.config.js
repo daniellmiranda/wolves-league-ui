a0e6ff293f428932e4fe74289903fade1f9f90c6
Daniel L. Miranda <danieldelacerdamiranda@proton.me>
2024-05-13 21:46:04 -0300

    chore: adds eslint and prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import wlEslintConfig from '@wolves-league/eslint-config';
+import wlConfig from '@wolves-league/eslint-config';
 
-const nodeConfig = wlEslintConfig.configs.node.map(configObject => ({ files: ['packages/**/*'], ...configObject }));
-const reactConfig = wlEslintConfig.configs.react.map(configObject => ({ files: ['packages/**/*'], ...configObject }));
+const nodeConfig = wlConfig.configs.node.map(configObject => ({ files: ['packages/tokens/**/*'], ...configObject }));
+const reactConfig = wlConfig.configs.react.map(configObject => ({ files: ['packages/react/**/*', 'packages/docs/**/*'], ...configObject }));
 
 /** @type {Array<import('@wolves-league/eslint-config').Config>} */
 export default [...nodeConfig, ...reactConfig];
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+import wlConfig from '@wolves-league/eslint-config';
+
+/** @type {import('@wolves-league/eslint-config').Config['prettier']} */
+export default {
+  ...wlConfig.configs.prettier,
+  plugins: ['prettier-plugin-tailwindcss'],
+  "tailwindFunctions": ['cn'],
+};
